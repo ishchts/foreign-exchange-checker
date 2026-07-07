ab69c93d0a864a5ac26cdc81843605abf78c21b2
Ilya Shchutskiy <i1ya1l@yandex.ru>
2026-07-07 12:31:51 +0300

    feat: add rate history range filter

--- frontend/src/pages/components/CurrencyInsights.tsx
+++ frontend/src/pages/components/CurrencyInsights.tsx
@@ -3,31 +3,56 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/shared/ui/tabs";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { RateHistoryMetrics } from "./RateHistoryMetrics";
+import { RateHistoryRangeFilter, RateRange } from "./RateHistoryRangeFilter";
 
-const date = new Date();
-const fromData = new Date();
-fromData.setMonth(date.getMonth() - 1);
+const getDateRange = (param: RateRange) => {
+  const today = new Date();
+  const fromDate = new Date(today);
+  if (param === "1D") {
+    fromDate.setDate(today.getDate() - 1);
+  }
+  if (param === "1W") {
+    fromDate.setDate(today.getDate() - 7);
+  }
+  if (param === "1M") {
+    fromDate.setMonth(today.getMonth() - 1);
+  }
+  if (param === "3M") {
+    fromDate.setMonth(today.getMonth() - 3);
+  }
+  if (param === "1Y") {
+    fromDate.setFullYear(today.getFullYear() - 1);
+  }
+  if (param === "5Y") {
+    fromDate.setFullYear(today.getFullYear() - 5);
+  }
 
+  return {
+    from: fromDate.toISOString().split("T")[0],
+    to: today.toISOString().split("T")[0],
+  };
+};
 export const CurrencyInsights = () => {
-  const [dates] = useState({
-    from: fromData.toISOString().split("T")[0],
-    to: date.toISOString().split("T")[0],
-  });
+  const [activeRange, setActiveRange] = useState<RateRange>("1M");
+  const handleActiveRange = (param: RateRange) => {
+    setActiveRange(param);
+  };
 
+  const dateRange = getDateRange(activeRange);
   const { data: rateHistory } = useQuery({
-    queryKey: ["rate-history"],
+    queryKey: ["rate-history", "USD", "EUR", dateRange.from, dateRange.to],
     queryFn: async () => {
       const { data } = await rates.getRates({
         base: "USD",
         quotes: "EUR",
-        from: dates.from,
-        to: dates.to,
+        from: dateRange.from,
+        to: dateRange.to,
       });
 
       return data;
     },
   });
-
+  console.log('rateHistory', rateHistory);
   return (
     <section className="mt-8.25" aria-label="Currency insights">
       <Tabs defaultValue="HISTORY">
@@ -53,9 +78,16 @@ export const CurrencyInsights = () => {
         </TabsList>
 
         <TabsContent value="HISTORY">
-          {rateHistory && rateHistory.length > 1 && (
-            <RateHistoryMetrics rate={rateHistory} />
-          )}
+          <div className="flex items-center">
+            {rateHistory && rateHistory.length > 1 && (
+              <RateHistoryMetrics rate={rateHistory} />
+            )}
+            <RateHistoryRangeFilter
+              className="ml-auto mr-0"
+              value={activeRange}
+              onChange={handleActiveRange}
+            />
+          </div>
         </TabsContent>
         <TabsContent value="COMPARE">COMPARE</TabsContent>
         <TabsContent value="FAVORITES">FAVORITES</TabsContent>
---
+++ frontend/src/pages/components/RateHistoryRangeFilter.tsx
@@ -0,0 +1,50 @@
+import { cn } from "@/shared/lib/cn";
+
+export type RateRange = "1D" | "1W" | "1M" | "3M" | "1Y" | "5Y";
+
+type RateHistoryRangeFilterProps = {
+  value: RateRange;
+  onChange: (value: RateRange) => void;
+  className?: string;
+};
+
+type Item = {
+    label: RateRange
+}
+
+const items: Item[] = [
+  { label: "1D" },
+  { label: "1W" },
+  { label: "1M" },
+  { label: "3M" },
+  { label: "1Y" },
+  { label: "5Y" },
+];
+
+export const RateHistoryRangeFilter: React.FC<RateHistoryRangeFilterProps> = ({
+  className,
+  value,
+  onChange
+}) => {
+  return (
+    <div
+      className={cn(
+        "bg-neutral-700 h-[42px] flex typography-preset-5 text-neutral-200 rounded-8",
+        className,
+      )}
+    >
+      {items.map((el) => (
+        <button
+          key={el.label}
+          type="button"
+          className={cn("rounded-8 w-[47px] cursor-pointer", {
+            ["text-white bg-neutral-500"]: value === el.label,
+          })}
+          onClick={() => onChange(el.label)}
+        >
+          {el.label}
+        </button>
+      ))}
+    </div>
+  );
+};
