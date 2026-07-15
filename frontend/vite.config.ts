83a60ccc12a04c089fd9a8aa45af8765af933cc1
Ilya Shchutskiy <i1ya1l@yandex.ru>
2026-07-15 14:48:02 +0300

    chore: configure GitHub Pages deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/foreign-exchange-checker/",
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
